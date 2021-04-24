df9c7314cbf8fa9d7ce83c914a536b4f37a6d809
Eternal36 <33805342+Eternal36@users.noreply.github.com>
2021-04-24 17:48:19 +0300

    Initial Commit

---
+++ Sunny/Supporting files/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Sunny
+//
+//  Created by Дмитрий Бельков on 23.04.2021.
+//  Copyright © 2021 Ivan Akulov. All rights reserved.
+//
+
+import Foundation
+
+let apiKey = "28f3ab66b7490192ad00d2a56a159dd1"
